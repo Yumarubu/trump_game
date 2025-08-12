8bb38ade42bd3a8fb11f58b22d8c7a0cc15eecf9
Yumarubu <241208@st.yoshida-g.ac.jp>
2025-08-12 12:49:43 +0900

    #5 5. ゲームを繰り返し遊べるようにする。 ゲームを終了させるための処理も併せて実装する。

--- trump_game/Program.cs
+++ trump_game/Program.cs
@@ -10,108 +10,146 @@ namespace trump_game
     {
         static void Main(string[] args)
         {
-            //まずはタイトル表示からの1～4の数字を4回分入力する
-            Console.WriteLine("**トランプゲーム**");
-            Console.WriteLine("1～4の値を入力してください");
-
-            //配列を用意
-            int[] trumps = new int[4];
-
-            for (int i = 0; i < 4; i++) 
+            //繰り返し遊べるようにwhile文でくくる
+            while (true) 
             {
-                Console.Write($"{i + 1}番目のカードの数字を入力してください > ");
-                string trumpNum = Console.ReadLine();
+                //まずはタイトル表示からの1～4の数字を4回分入力する
+                Console.WriteLine("**トランプゲーム**");
+                Console.WriteLine("1～4の値を入力してください");
 
-                //数字のデータを格納する変数を用意
-                int trumpNumber;
-                bool numJuage = int.TryParse(trumpNum, out trumpNumber);
+                //配列を用意
+                int[] trumps = new int[4];
 
-                //結果に応じて、次に進む
-                while (true) 
+                for (int i = 0; i < 4; i++)
                 {
-                    if (trumpNumber < 1 || trumpNumber > 4)
+                    Console.Write($"{i + 1}番目のカードの数字を入力してください > ");
+                    string trumpNum = Console.ReadLine();
+
+                    //数字のデータを格納する変数を用意
+                    int trumpNumber;
+                    bool numJuage = int.TryParse(trumpNum, out trumpNumber);
+
+                    //結果に応じて、次に進む
+                    while (true)
                     {
-                        Console.WriteLine("入力値が違います");
-                        Console.Write("もう一度、カードの値を入力してください > ");
-                        trumpNum = Console.ReadLine();
-                        numJuage = int.TryParse(trumpNum, out trumpNumber);
+                        if (!numJuage || trumpNumber < 1 || trumpNumber > 4)
+                        {
+                            Console.WriteLine("入力値が違います");
+                            Console.Write("もう一度、カードの値を入力してください > ");
+                            trumpNum = Console.ReadLine();
+                            numJuage = int.TryParse(trumpNum, out trumpNumber);
+                        }
+                        else if (numJuage)
+                        {
+                            trumps[i] = trumpNumber;
+                            break;
+                        }
                     }
-                    else if (numJuage) 
-                    { 
-                        trumps[i] = trumpNumber;
+                }
+
+                //入力した4つのトランプのカードの番号を確認し、判定する
+                if (trumps[0] == trumps[1] && trumps[0] == trumps[2] && trumps[0] == trumps[3])
+                {
+                    Console.WriteLine("フォーカード");
+                }
+                else if (trumps[0] == trumps[1] && trumps[0] == trumps[2] && trumps[0] != trumps[3] || trumps[1] == trumps[2] && trumps[1] == trumps[3] && trumps[1] != trumps[0] || trumps[2] == trumps[0] && trumps[2] == trumps[3] && trumps[2] != trumps[1] || trumps[3] == trumps[0] && trumps[3] == trumps[1] && trumps[3] != trumps[2])
+                {
+                    Console.WriteLine("スリーカード");
+                }
+                else if (trumps[0] == trumps[1] && trumps[2] == trumps[3] || trumps[0] == trumps[3] && trumps[1] == trumps[2] || trumps[0] == trumps[2] && trumps[1] == trumps[3])
+                {
+                    Console.WriteLine("ツーペア");
+                }
+                else if (trumps[0] == trumps[1] && trumps[0] != trumps[2] && trumps[0] != trumps[3] || trumps[1] == trumps[2] && trumps[1] != trumps[3] && trumps[1] != trumps[0] || trumps[2] == trumps[3] && trumps[2] != trumps[1] && trumps[2] != trumps[0] || trumps[0] == trumps[3] && trumps[0] != trumps[1] && trumps[0] != trumps[2] || trumps[0] == trumps[2] && trumps[0] != trumps[1] && trumps[0] != trumps[3] || trumps[1] == trumps[3] && trumps[1] != trumps[0] && trumps[1] != trumps[2])
+                {
+                    Console.WriteLine("ワンペア");
+                }
+                else
+                {
+                    Console.WriteLine("ノーペア");
+                }
+
+                Console.WriteLine();
+                Console.ReadLine();
+
+                //改めて判定結果を出力
+                Console.WriteLine("判定結果");
+                if (trumps[0] == trumps[1] && trumps[0] == trumps[2] && trumps[0] == trumps[3])
+                {
+                    Console.WriteLine($"一つ目のトランプの数値:{trumps[0]}");
+                    Console.WriteLine($"二つ目のトランプの数値:{trumps[1]}");
+                    Console.WriteLine($"三つ目のトランプの数値:{trumps[2]}");
+                    Console.WriteLine($"四つ目のトランプの数値:{trumps[3]}");
+                    Console.WriteLine("4つの数値が全て同じなのでフォーカード");
+                }
+                else if (trumps[0] == trumps[1] && trumps[0] == trumps[2] && trumps[0] != trumps[3] || trumps[1] == trumps[2] && trumps[1] == trumps[3] && trumps[1] != trumps[0] || trumps[2] == trumps[0] && trumps[2] == trumps[3] && trumps[2] != trumps[1] || trumps[3] == trumps[0] && trumps[3] == trumps[1] && trumps[3] != trumps[2])
+                {
+                    Console.WriteLine($"一つ目のトランプの数値:{trumps[0]}");
+                    Console.WriteLine($"二つ目のトランプの数値:{trumps[1]}");
+                    Console.WriteLine($"三つ目のトランプの数値:{trumps[2]}");
+                    Console.WriteLine($"四つ目のトランプの数値:{trumps[3]}");
+                    Console.WriteLine("4つの内、3つの数値が同じなのでスリーカード");
+                }
+                else if (trumps[0] == trumps[1] && trumps[2] == trumps[3] || trumps[0] == trumps[3] && trumps[1] == trumps[2] || trumps[0] == trumps[2] && trumps[1] == trumps[3])
+                {
+                    Console.WriteLine($"一つ目のトランプの数値:{trumps[0]}");
+                    Console.WriteLine($"二つ目のトランプの数値:{trumps[1]}");
+                    Console.WriteLine($"三つ目のトランプの数値:{trumps[2]}");
+                    Console.WriteLine($"四つ目のトランプの数値:{trumps[3]}");
+                    Console.WriteLine("4つの内、2つの数値が綺麗に2つ揃っているのでツーペア");
+                }
+                else if (trumps[0] == trumps[1] && trumps[0] != trumps[2] && trumps[0] != trumps[3] || trumps[1] == trumps[2] && trumps[1] != trumps[3] && trumps[1] != trumps[0] || trumps[2] == trumps[3] && trumps[2] != trumps[1] && trumps[2] != trumps[0] || trumps[0] == trumps[3] && trumps[0] != trumps[1] && trumps[0] != trumps[2] || trumps[0] == trumps[2] && trumps[0] != trumps[1] && trumps[0] != trumps[3] || trumps[1] == trumps[3] && trumps[1] != trumps[0] && trumps[1] != trumps[2])
+                {
+                    Console.WriteLine($"一つ目のトランプの数値:{trumps[0]}");
+                    Console.WriteLine($"二つ目のトランプの数値:{trumps[1]}");
+                    Console.WriteLine($"三つ目のトランプの数値:{trumps[2]}");
+                    Console.WriteLine($"四つ目のトランプの数値:{trumps[3]}");
+                    Console.WriteLine("4つの内、2つの数値が同じなのでワンペア");
+                }
+                else
+                {
+                    Console.WriteLine($"一つ目のトランプの数値:{trumps[0]}");
+                    Console.WriteLine($"二つ目のトランプの数値:{trumps[1]}");
+                    Console.WriteLine($"三つ目のトランプの数値:{trumps[2]}");
+                    Console.WriteLine($"四つ目のトランプの数値:{trumps[3]}");
+                    Console.WriteLine("4つの内、どの数値も同じじゃないのでノーペア");
+                }
+
+                Console.WriteLine();
+                Console.ReadLine();
+
+                Console.WriteLine("もう一度、遊んでみる?");
+                Console.Write("(はい→1 いいえ→0) > ");
+                string selectPlay = Console.ReadLine();
+                int selectPlayNum;
+
+                bool selectJuage = int.TryParse(selectPlay, out selectPlayNum);
+                while(true)
+                {
+                    if(!selectJuage || selectPlayNum < 0 || selectPlayNum > 1)
+                    {
+                        Console.WriteLine("入力範囲外なので、もう一度入力してください");
+                        Console.WriteLine("もう一度、遊んでみる?");
+                        Console.Write("(はい→1 いいえ→0) > ");
+                        selectPlay = Console.ReadLine();
+                        selectJuage = int.TryParse(selectPlay, out selectPlayNum);
+                        Console.WriteLine();
+                    }
+                    else if(selectJuage)
+                    {
+                        Console.WriteLine();
                         break;
                     }
                 }
+
+                if(selectPlayNum == 0)
+                {
+                    Console.WriteLine("OK!また遊ぼうね！");
+                    break;
+                }
             }
 
-            //入力した4つのトランプのカードの番号を確認し、判定する
-            if (trumps[0] == trumps[1] && trumps[0] == trumps[2] && trumps[0] == trumps[3])
-            {
-                Console.WriteLine("フォーカード");
-            }
-            else if (trumps[0] == trumps[1] && trumps[0] == trumps[2] && trumps[0] != trumps[3] || trumps[1] == trumps[2] && trumps[1] == trumps[3] && trumps[1] != trumps[0] || trumps[2]  == trumps[0] && trumps[2] == trumps[3] && trumps[2] != trumps[1] || trumps[3] == trumps[0] && trumps[3] == trumps[1] && trumps[3] != trumps[2])
-            {
-                Console.WriteLine("スリーカード");
-            }
-            else if (trumps[0] == trumps[1] && trumps[2] == trumps[3] || trumps[0] == trumps[3] && trumps[1] == trumps[2] || trumps[0] == trumps[2] && trumps[1] == trumps[3])
-            {
-                Console.WriteLine("ツーペア");
-            }
-            else if (trumps[0] == trumps[1] && trumps[0] != trumps[2] && trumps[0] != trumps[3] || trumps[1] == trumps[2] && trumps[1] != trumps[3] && trumps[1] != trumps[0] || trumps[2] == trumps[3] && trumps[2] != trumps[1] && trumps[2] != trumps[0] || trumps[0] == trumps[3] && trumps[0] != trumps[1] && trumps[0] != trumps[2] || trumps[0] == trumps[2] && trumps[0] != trumps[1] && trumps[0] != trumps[3] || trumps[1] == trumps[3] && trumps[1] != trumps[0] && trumps[1] != trumps[2])
-            {
-                Console.WriteLine("ワンペア");
-            }
-            else
-            {
-                Console.WriteLine("ノーペア");
-            }
-
-            Console.ReadLine();
-
-            //改めて判定結果を出力
-            Console.WriteLine("判定結果");
-            if (trumps[0] == trumps[1] && trumps[0] == trumps[2] && trumps[0] == trumps[3])
-            {
-                Console.WriteLine($"一つ目のトランプの数値:{trumps[0]}");
-                Console.WriteLine($"二つ目のトランプの数値:{trumps[1]}");
-                Console.WriteLine($"三つ目のトランプの数値:{trumps[2]}");
-                Console.WriteLine($"四つ目のトランプの数値:{trumps[3]}");
-                Console.WriteLine("4つの数値が全て同じなのでフォーカード");
-            }
-            else if (trumps[0] == trumps[1] && trumps[0] == trumps[2] && trumps[0] != trumps[3] || trumps[1] == trumps[2] && trumps[1] == trumps[3] && trumps[1] != trumps[0] || trumps[2] == trumps[0] && trumps[2] == trumps[3] && trumps[2] != trumps[1] || trumps[3] == trumps[0] && trumps[3] == trumps[1] && trumps[3] != trumps[2])
-            {
-                Console.WriteLine($"一つ目のトランプの数値:{trumps[0]}");
-                Console.WriteLine($"二つ目のトランプの数値:{trumps[1]}");
-                Console.WriteLine($"三つ目のトランプの数値:{trumps[2]}");
-                Console.WriteLine($"四つ目のトランプの数値:{trumps[3]}");
-                Console.WriteLine("4つの内、3つの数値が同じなのでスリーカード");
-            }
-            else if (trumps[0] == trumps[1] && trumps[2] == trumps[3] || trumps[0] == trumps[3] && trumps[1] == trumps[2] || trumps[0] == trumps[2] && trumps[1] == trumps[3])
-            {
-                Console.WriteLine($"一つ目のトランプの数値:{trumps[0]}");
-                Console.WriteLine($"二つ目のトランプの数値:{trumps[1]}");
-                Console.WriteLine($"三つ目のトランプの数値:{trumps[2]}");
-                Console.WriteLine($"四つ目のトランプの数値:{trumps[3]}");
-                Console.WriteLine("4つの内、2つの数値が綺麗に2つ揃っているのでツーペア");
-            }
-            else if (trumps[0] == trumps[1] && trumps[0] != trumps[2] && trumps[0] != trumps[3] || trumps[1] == trumps[2] && trumps[1] != trumps[3] && trumps[1] != trumps[0] || trumps[2] == trumps[3] && trumps[2] != trumps[1] && trumps[2] != trumps[0] || trumps[0] == trumps[3] && trumps[0] != trumps[1] && trumps[0] != trumps[2] || trumps[0] == trumps[2] && trumps[0] != trumps[1] && trumps[0] != trumps[3] || trumps[1] == trumps[3] && trumps[1] != trumps[0] && trumps[1] != trumps[2])
-            {
-                Console.WriteLine($"一つ目のトランプの数値:{trumps[0]}");
-                Console.WriteLine($"二つ目のトランプの数値:{trumps[1]}");
-                Console.WriteLine($"三つ目のトランプの数値:{trumps[2]}");
-                Console.WriteLine($"四つ目のトランプの数値:{trumps[3]}");
-                Console.WriteLine("4つの内、2つの数値が同じなのでワンペア");
-            }
-            else
-            {
-                Console.WriteLine($"一つ目のトランプの数値:{trumps[0]}");
-                Console.WriteLine($"二つ目のトランプの数値:{trumps[1]}");
-                Console.WriteLine($"三つ目のトランプの数値:{trumps[2]}");
-                Console.WriteLine($"四つ目のトランプの数値:{trumps[3]}");
-                Console.WriteLine("4つの内、どの数値も同じじゃないのでノーペア");
-            }
-
-            Console.ReadLine();
+            Console.ReadLine() ;
         }
     }
 }
